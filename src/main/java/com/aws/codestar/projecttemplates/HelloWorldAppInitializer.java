fbf7893e3cac95761e1df8f3ec5ff899cfb4ab53
jamescurrandickinson <34398675+jamescurrandickinson@users.noreply.github.com>
2017-12-09 16:13:33 +0000

    Initial checkin

--- src/main/java/com/aws/codestar/projecttemplates/HelloWorldAppInitializer.java
+++ src/main/java/com/aws/codestar/projecttemplates/HelloWorldAppInitializer.java
@@ -24,7 +24,7 @@ public class HelloWorldAppInitializer extends AbstractAnnotationConfigDispatcher
     @Override
     protected String[] getServletMappings() {
         return new String[] {
-                "/"
+                "/rest"
         };
     }
 
