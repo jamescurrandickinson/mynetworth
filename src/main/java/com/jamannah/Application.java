9fb5e2842b6adb8c28d92245852c3f4e292e4ff8
jamescurrandickinson <34398675+jamescurrandickinson@users.noreply.github.com>
2019-01-05 16:10:09 +0000

    Initial checkin

--- src/main/java/com/jamannah/Application.java
+++ src/main/java/com/jamannah/Application.java
@@ -3,7 +3,6 @@ package com.jamannah;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
